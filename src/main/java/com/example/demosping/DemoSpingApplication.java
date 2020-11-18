1691944a54c9e1d5458f57f124b6358196c912b5
up1 <somkiat.p@gmail.com>
2020-11-18 14:58:27 +0700

    Initial data

--- src/main/java/com/example/demosping/DemoSpingApplication.java
+++ src/main/java/com/example/demosping/DemoSpingApplication.java
@@ -1,11 +1,29 @@
 package com.example.demosping;
 
+import com.example.demosping.account.Account;
+import com.example.demosping.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DemoSpingApplication {
+
+	@Autowired AccountRepository repository;
+
+//	@Profile({"testing"})
+	@PostConstruct
+	public void initialDataForTesting() {
+		Account somkiat = new Account();
+		somkiat.setAccountId(1);
+		somkiat.setAccountName("Somkiat for test");
+		somkiat.setAge(100);
+		repository.save(somkiat);
+	}
 
 	public static void main(String[] args) {
 		//gradlew bootRun
